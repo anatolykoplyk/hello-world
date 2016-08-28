513b7afbf8353e268b607c61a824fbc277985208
Anatoliy Koplik (Consultant) <anatoliy.koplik@consultant.volvo.com>
2016-08-28 17:17:30 +0300

    Peak Finding

---
+++ Algorithms/Algorithms/Helpers/MatrixGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Helpers
+{
+	class MatrixGenerator
+	{
+
+
+		private int[][] CreateRandomProblem(int cols = 10, int rows = 10, int maxElement = 1000)
+		{
+			var rnd = new Random();
+			var result = new int[][] {};
+
+			for (var m = 0; m < cols; m++)
+			{
+				var row = new int[] {};
+				for (var n = 0; n < rows; n++)
+				{
+					row[n] = rnd.Next(0, maxElement);
+				}
+				result[m] = row;
+			}
+
+			return result;
+		}
+	}
+}
